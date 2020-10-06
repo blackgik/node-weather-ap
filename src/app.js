c1da822f49e1bf4675a487c4e227e1def2064182
echendu Chinedum <echendu.chinedum@gmail.com>
2020-10-06 13:33:29 +0100

    commit for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const geocode = require('./utils/geoCode');
 
 const app = express();
 
+// creating port for the heroku app to listen to
+const port = process.env.PORT || 3000
+
 // define paths for config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -102,6 +105,6 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('server is up on port ' + port)
 })
